73161e52ab2931a2a55c0b24ece5ca42913af0cd
sharozraees802 <sharozraees802@gmail.com>
2023-12-16 14:00:57 +0500

    delete all module by create manually

--- src/root.module.ts
+++ src/root.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users.module';
-import { OrdersModule } from './orders.module';
-import { ChatModule } from './chat.module';
 
 @Module({
-  imports: [UsersModule, OrdersModule, ChatModule],
+  imports: [],
   controllers: [],
   providers: [],
   exports: [],
