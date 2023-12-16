c03a600fa1536e26d6fb443c40e7726cb9338873
sharozraees802 <sharozraees802@gmail.com>
2023-12-16 13:53:04 +0500

    feature3 add in chat module

--- src/chat.module.ts
+++ src/chat.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { Feature3Module } from './feature3.module';
 
 @Module({
-  imports: [],
+  imports: [Feature3Module],
   controllers: [],
   providers: [],
   exports: [],
--- src/orders.module.ts
+++ src/orders.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { Feature1Module } from './feature1.module';
 import { Feature2Module } from './feature2.module';
-import { Feature3Module } from './feature3.module';
 
 @Module({
-  imports: [Feature1Module, Feature2Module, Feature3Module],
+  imports: [Feature1Module, Feature2Module],
   controllers: [],
   providers: [],
   exports: [],
