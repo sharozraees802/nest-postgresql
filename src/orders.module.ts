40f3eb73a5fc7486bf2d174812b01d99380cff49
sharozraees802 <sharozraees802@gmail.com>
2023-12-16 13:43:29 +0500

    added new module according to doc

---
+++ src/orders.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class OrdersModule {
+  constructor() {
+    console.log('OrdersModule');
+  }
+}
---
+++ src/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class UsersModule {
+  constructor() {
+    console.log('UsersModule');
+  }
+}
