ee3c4cfe700b3198f5ca94b4cb5299547fb98c49
sharozraees802 <sharozraees802@gmail.com>
2023-12-18 12:45:10 +0500

    create a services

---
+++ src/book.service.ts
@@ -0,0 +1,22 @@
+export class BookService {
+  addBook(): string {
+    return 'This will be add book';
+  }
+  //delete book
+  deleteBook(): string {
+    return 'This is for deleteing book';
+  }
+  //update book
+  updateBook(): string {
+    return 'This is for updating book';
+  }
+  //find all book
+  findAllBook(): string {
+    return 'This is for finding all book';
+  }
+
+  findBookById(bookId: string): string {
+    console.log(bookId);
+    return `This is for finding book by id ${bookId}`;
+  }
+}
