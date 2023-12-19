bde908aa4e4229780208d02de0aae8183443196d
sharozraees802 <sharozraees802@gmail.com>
2023-12-19 14:51:39 +0500

    check addition thing in request object

--- src/book/book.middleware.ts
+++ src/book/book.middleware.ts
@@ -4,7 +4,13 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class BookMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('class base BookMiddleware');
+    const protocol = req.protocol;
+    const host = req.get('host');
+    const url = req.originalUrl;
+    const method = req.method;
+    const date = new Date().toDateString();
+
+    console.log(`${method} ${protocol}://${host}${url} ${date}`);
     next();
   }
 }
